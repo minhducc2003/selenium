e192132121ca206d1964a907959c05c8e8efb108
stormz <21111062080@hunre.edu.vn>
2024-12-01 21:56:13 +0700

    update export report 2

--- src/test/java/testcases/BaseTest.java
+++ src/test/java/testcases/BaseTest.java
@@ -2,30 +2,110 @@ package testcases;
 
 import common.BaseSetup;
 import helpers.Keyword;
+import helpers.PropertiesFile;
+import com.relevantcodes.extentreports.ExtentReports;
+import com.relevantcodes.extentreports.ExtentTest;
+import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
-import helpers.PropertiesFile;
+import org.testng.annotations.BeforeSuite;
 
+import java.io.File;
 import java.text.SimpleDateFormat;
 import java.time.Duration;
 import java.util.Date;
 
 public class BaseTest extends Keyword {
+    // Khai báo ExtentReports
+    protected static ExtentReports extent;
+    protected static ExtentTest test;
+
+    @BeforeSuite
+    public void setUpReport() {
+        // Đảm bảo properties được load
+        PropertiesFile.setPropertiesFile();
+
+        // Tạo thư mục reports nếu chưa tồn tại
+        File reportDir = new File("src/test/resources/reports");
+        if (!reportDir.exists()) {
+            reportDir.mkdirs();
+        }
+
+        // Khởi tạo ExtentReports
+        String reportName = "AutomationReport_" +
+                new SimpleDateFormat("yyyy-MM-dd_HHmmss").format(new Date()) + ".html";
+        extent = new ExtentReports("src/test/resources/reports/" + reportName, true);
+
+        // Thêm thông tin hệ thống
+        extent.addSystemInfo("Host Name", "LocalHost")
+                .addSystemInfo("Environment", "QA")
+                .addSystemInfo("Browser", getSafeProperty("browser"))
+                .addSystemInfo("URL", getSafeProperty("url"));
+    }
 
     @BeforeMethod
-    public void setup() {
-        PropertiesFile.setPropertiesFile();
-        driver = new BaseSetup().setupDriver(PropertiesFile.getPropValue("browser"));
-        wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutWaitForPageLoaded)); // correct constructor for WebDriverWait
-        Date date = new Date();
-        String LogDate = new SimpleDateFormat("yyyy-MM-dd_HHmmss").format(date);
-        System.setProperty("logFilename", LogDate);
+    public void setup(java.lang.reflect.Method method) {
+        // Chuẩn bị driver
+        driver = new BaseSetup().setupDriver(getSafeProperty("browser"));
+        wait = new WebDriverWait(driver, Duration.ofSeconds(timeoutWaitForPageLoaded));
+
+        // Bắt đầu test trong ExtentReports
+        test = extent.startTest(method.getName(), "Chi tiết test case: " + method.getName());
     }
 
     @AfterMethod
-    public void tearDown() {
+    public void tearDown(ITestResult result) {
+        // Ghi log kết quả test
+        switch (result.getStatus()) {
+            case ITestResult.FAILURE -> {
+                test.log(LogStatus.FAIL, "Test Case Failed: " + result.getName());
+                test.log(LogStatus.FAIL, "Error: " + result.getThrowable());
+            }
+            case ITestResult.SKIP -> test.log(LogStatus.SKIP, "Test Case Skipped: " + result.getName());
+            case ITestResult.SUCCESS -> test.log(LogStatus.PASS, "Test Case Passed: " + result.getName());
+        }
+
+        // Kết thúc test
+        extent.endTest(test);
+
+        // Đóng trình duyệt
         driver.quit();
+    }
+
+    @AfterSuite
+    public void endReport() {
+        // Lưu và đóng báo cáo
+        extent.flush();
+        extent.close();
+    }
+
+    // Các hàm tiện ích cho logging
+    public void logTestStart(String testName, String name, String customerGroup, String phoneNumber) {
+        test.log(LogStatus.INFO, "Bắt đầu test: " + testName);
+        test.log(LogStatus.INFO, String.format("Dữ liệu: Name: %s, Customer Group: %s, Phone Number: %s",
+                name, customerGroup, phoneNumber));
+    }
+
+    public void logTestNavigation(String url) {
+        test.log(LogStatus.INFO, "Điều hướng đến: " + url);
+    }
+
+    public void logTestSuccess(String testName) {
+        test.log(LogStatus.PASS, "Test " + testName + " thành công");
+    }
+
+    public void logTestFailure(String testName, Exception e) {
+        test.log(LogStatus.FAIL, "Test " + testName + " thất bại");
+        test.log(LogStatus.FAIL, "Chi tiết lỗi: " + e.getMessage());
+    }
+
+    // Hàm tiện ích để lấy giá trị an toàn từ properties
+    private String getSafeProperty(String key) {
+        String value = PropertiesFile.getPropValue(key);
+        return value != null ? value : "Not Specified";
     }
 }
--- src/test/java/testcases/TestNhap/TestNhapDuLieu.java
+++ src/test/java/testcases/TestNhap/TestNhapDuLieu.java
@@ -2,6 +2,7 @@ package testcases.TestNhap;
 
 import com.opencsv.CSVReader;
 import com.opencsv.exceptions.CsvException;
+import com.relevantcodes.extentreports.LogStatus;
 import helpers.PropertiesFile;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
@@ -14,81 +15,168 @@ import java.util.List;
 
 public class TestNhapDuLieu extends BaseTest {
     private TestPage testPage;
-    @DataProvider(name = "testData")
-    public Object[][] readCsvData() throws IOException, CsvException {
-        // Đọc file CSV
-        CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/datatest.csv"));
-        List<String[]> allRows = csvReader.readAll();
-        Object[][] data = new Object[allRows.size()][3];  // Vì có 3 cột: name, customerGroup, phoneNumber
-        for (int i = 0; i < allRows.size(); i++) {
-            data[i][0] = allRows.get(i)[0];  // name
-            data[i][1] = allRows.get(i)[1];  // customerGroup
-            data[i][2] = allRows.get(i)[2];  // phoneNumber
-        }
-        return data;
-    }
-    @Test(priority = 1, description = "Nhập dữ liệu thành công", dataProvider = "testData")
-    public void NhapDuLieuThanhCong(String name, String customerGroup, String phoneNumber){
-        testPage = new TestPage(driver);
-        navigateToURL(PropertiesFile.getPropValue("url"));
-        testPage.boQuaTatCa(name, customerGroup, phoneNumber);
-        // Thực hiện các kiểm tra sau khi nhập dữ liệu
-        // testPage.thayThongBaoThanhCong("Thông báo thành công");
-    }
+//    @DataProvider(name = "testData")
+//    public Object[][] readCsvData() throws IOException, CsvException {
+//        // Đọc file CSV
+//        CSVReader csvReader = new CSVReader(new FileReader("src/test/resources/datatest.csv"));
+//        List<String[]> allRows = csvReader.readAll();
+//        Object[][] data = new Object[allRows.size() - 1][3];  // Bỏ qua dòng tiêu đề
+//        for (int i = 1; i < allRows.size(); i++) {
+//            data[i-1][0] = allRows.get(i)[0];  // name
+//            data[i-1][1] = allRows.get(i)[1];  // customerGroup
+//            data[i-1][2] = allRows.get(i)[2];  // phoneNumber
+//        }
+//        return data;
+//    }
+//    @Test(priority = 1, description = "Nhập dữ liệu thành công", dataProvider = "testData")
+//    public void NhapDuLieuThanhCong(String name, String customerGroup, String phoneNumber) {
+//        try {
+//            test.log(LogStatus.INFO, "Bắt đầu test nhập dữ liệu");
+//            test.log(LogStatus.INFO, "Dữ liệu: " +
+//                    "Name: " + name +
+//                    ", Customer Group: " + customerGroup +
+//                    ", Phone Number: " + phoneNumber);
+//
+//            testPage = new TestPage(driver);
+//            navigateToURL(PropertiesFile.getPropValue("url"));
+//            test.log(LogStatus.INFO, "Điều hướng đến: " + PropertiesFile.getPropValue("url"));
+//
+//            testPage.boQuaTatCa(name, customerGroup, phoneNumber);
+//            test.log(LogStatus.INFO, "Nhập dữ liệu thành công");
+//            test.log(LogStatus.PASS, "Test nhập dữ liệu thành công");
+//        } catch (Exception e) {
+//            // Nếu có lỗi, log và fail test
+//            test.log(LogStatus.FAIL, "Test nhập dữ liệu thất bại");
+//            test.log(LogStatus.FAIL, "Chi tiết lỗi: " + e.getMessage());
+//
+//            throw e;
+//        }
+//    }
 
 //    @Test(priority = 1, description = "Nhập dữ liệu thành công")
 //    public void NhapDuLieuThanhCong(){
-//        testPage = new TestPage(driver);
-//        navigateToURL(PropertiesFile.getPropValue("url"));
-//        testPage.themThanhCong();
+//        String testName = "Nhập dữ liệu thành công";
+//        try {
+//            logTestStart(testName, "Trinh Minh Duc","Modern Trade","0368424724");
+//
+//            testPage = new TestPage(driver);
+//            String url = PropertiesFile.getPropValue("url");
+//            navigateToURL(url);
+//            logTestNavigation(url);
+//
+//            testPage.themThanhCong();
+//            testPage.thayThongBaoAlertThemThanhCong();
+//
+//            logTestSuccess(testName);
+//        }catch (Exception e){
+//            logTestFailure(testName,e);
+//            throw e;
+//        }
 //    }
-//
-//    @Test(priority = 2, description = "Nhập dữ liệu để trống tên combo box")
-//    public void NhapDuLieuFailCBBKhachHang() {
-//        testPage = new TestPage(driver);
-//        navigateToURL(PropertiesFile.getPropValue("url"));
-//        testPage.boQuaTatCa("Trinh Minh Duc", "", "0368424724");
-//
-//        testPage.thayThongBaoFailCBB("nhóm khách hàng không được để trống");
-//    }
-//
-//    @Test(priority = 3, description = "Nhập dữ liệu tên khách hàng sai format")
-//    public void NhapDuLieuFailTenKhachHangFormat() {
-//        testPage = new TestPage(driver);
-//        navigateToURL(PropertiesFile.getPropValue("url"));
-//        testPage.boQuaTatCa("123", "Modern Trade", "0368424724");
-//
-//        // Kiểm tra từng thông báo lỗi
-//        testPage.thayThongBaoFailTenKhachHangFormat("tên khách hàng chỉ chứa chữ cái, khoảng trắng và dấu");
-//    }
-//    @Test(priority = 4, description = "Nhập dữ liệu tên khách hàng sai size min")
-//    public void NhapDuLieuFailTenKhachHangSizeMin() {
-//        testPage = new TestPage(driver);
-//        navigateToURL(PropertiesFile.getPropValue("url"));
-//        testPage.boQuaTatCa("a", "Modern Trade", "0368424724");
-//
-//        // Kiểm tra từng thông báo lỗi
-//        testPage.thayThongBaoFailTenKhachHangSize("tên khách hàng phải từ 3-50 ký tự");
-//    }
-//    @Test(priority = 5, description = "Nhập dữ liệu tên khách hàng sai size max")
-//    public void NhapDuLieuFailTenKhachHangSizeMax() {
-//        testPage = new TestPage(driver);
-//        navigateToURL(PropertiesFile.getPropValue("url"));
-//        testPage.boQuaTatCa("abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc", "Modern Trade", "0368424724");
-//
-//        // Kiểm tra từng thông báo lỗi
-//        testPage.thayThongBaoFailTenKhachHangSize("tên khách hàng phải từ 3-50 ký tự");
-//    }
-//    @Test(priority = 6, description = "Nhập dữ liệu để trống số điện thoại")
-//    public void NhapDuLieuFailSoDienThoaiTrong() {
-//        testPage = new TestPage(driver);
-//        navigateToURL(PropertiesFile.getPropValue("url"));
-//        testPage.boQuaTatCa("Trinh Minh Duc", "Modern Trade", "");
-//
-//        // Kiểm tra từng thông báo lỗi
-//        testPage.thayThongBaoFailSoDienThoaiTrong("số điện thoại không được để trống");
-//        testPage.thayThongBaoFailSoDienThoaiFormat("số điện thoại phải bắt đầu bằng 0 và có độ dài 10-12 ký tự");
-//    }
+
+    @Test(priority = 2, description = "Nhập dữ liệu để trống tên combo box")
+    public void NhapDuLieuFailCBBKhachHang() {
+        String testName = "Nhập dữ liệu để trống tên combo box";
+        try {
+            logTestStart(testName, "Trinh Minh Duc","","0368424724");
+
+            testPage = new TestPage(driver);
+            String url = PropertiesFile.getPropValue("url");
+            navigateToURL(url);
+            logTestNavigation(url);
+
+            testPage.boQuaTatCa("Trinh Minh Duc","","0368424724");
+            testPage.thayThongBaoFailCBB("nhóm khách hàng không được để trống");
+
+            logTestSuccess(testName);
+        }catch (Exception e){
+            logTestFailure(testName,e);
+            throw e;
+        }
+    }
+
+    @Test(priority = 3, description = "Nhập dữ liệu tên khách hàng sai format")
+    public void NhapDuLieuFailTenKhachHangFormat() {
+
+        String testName = "Nhập dữ liệu tên khách hàng sai format";
+        try {
+            logTestStart(testName, "123","Modern Trade","0368424724");
+
+            testPage = new TestPage(driver);
+            String url = PropertiesFile.getPropValue("url");
+            navigateToURL(url);
+            logTestNavigation(url);
+
+            testPage.boQuaTatCa("123","Modern Trade","0368424724");
+            testPage.thayThongBaoFailTenKhachHangFormat("tên khách hàng chỉ chứa chữ cái, khoảng trắng và dấu");
+
+            logTestSuccess(testName);
+        }catch (Exception e){
+            logTestFailure(testName,e);
+            throw e;
+        }
+    }
+    @Test(priority = 4, description = "Nhập dữ liệu tên khách hàng sai size min")
+    public void NhapDuLieuFailTenKhachHangSizeMin() {
+        String testName = "Nhập dữ liệu tên khách hàng sai size min";
+        try {
+            logTestStart(testName, "a", "Modern Trade", "0368424724");
+
+            testPage = new TestPage(driver);
+            String url = PropertiesFile.getPropValue("url");
+            navigateToURL(url);
+            logTestNavigation(url);
+
+            testPage.boQuaTatCa("a", "Modern Trade", "0368424724");
+            testPage.thayThongBaoFailTenKhachHangSize("tên khách hàng phải từ 3-50 ký tự");
+
+            logTestSuccess(testName);
+        }catch (Exception e){
+            logTestFailure(testName,e);
+            throw e;
+        }
+    }
+    @Test(priority = 5, description = "Nhập dữ liệu tên khách hàng sai size max")
+    public void NhapDuLieuFailTenKhachHangSizeMax() {
+        String testName = "Nhập dữ liệu tên khách hàng sai size max";
+        try {
+            logTestStart(testName, "abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc", "Modern Trade", "0368424724");
+
+            testPage = new TestPage(driver);
+            String url = PropertiesFile.getPropValue("url");
+            navigateToURL(url);
+            logTestNavigation(url);
+
+            testPage.boQuaTatCa("abcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabcabc", "Modern Trade", "0368424724");
+            testPage.thayThongBaoFailTenKhachHangSize("tên khách hàng phải từ 3-50 ký tự");
+
+            logTestSuccess(testName);
+        }catch (Exception e){
+            logTestFailure(testName,e);
+            throw e;
+        }
+    }
+    @Test(priority = 6, description = "Nhập dữ liệu để trống số điện thoại")
+    public void NhapDuLieuFailSoDienThoaiTrong() {
+        String testName = "Nhập dữ liệu tên khách hàng sai size max";
+        try {
+            logTestStart(testName, "Trinh Minh Duc", "Modern Trade", "");
+
+            testPage = new TestPage(driver);
+            String url = PropertiesFile.getPropValue("url");
+            navigateToURL(url);
+            logTestNavigation(url);
+
+            testPage.boQuaTatCa("Trinh Minh Duc", "Modern Trade", "");
+            testPage.thayThongBaoFailSoDienThoaiTrong("số điện thoại không được để trống");
+            testPage.thayThongBaoFailSoDienThoaiFormat("số điện thoại phải bắt đầu bằng 0 và có độ dài 10-12 ký tự");
+
+            logTestSuccess(testName);
+        }catch (Exception e){
+            logTestFailure(testName,e);
+            throw e;
+        }
+    }
 
 
 }
